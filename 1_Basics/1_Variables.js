aea50e73fa9db67f8823d6eb1d5e673ad5fabfee
Dishant Pal <dishantmrt02@gmail.com>
2024-09-16 20:46:44 +0530

    1_Variables

--- 1_Basics/1_Variables.js
+++ 1_Basics/1_Variables.js
@@ -1,1 +1,20 @@
-console.log("Dishant Pal")
+const accountId = 4163
+let accountEmail = "bond007@gmail.com"
+var accountPassword = "1234567"
+accountCity = "Jaipur"
+
+//accountId = 2 //since accId is const/constant that cannot be changed which is not allowed
+
+accountEmail = "b7b7@gmail.com"
+accountPassword = "654123"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope i.e., " {} " and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
