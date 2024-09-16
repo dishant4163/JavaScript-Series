a452bf0d05af0d245f17e4a9dd42428a38bb586a
Dishant Pal <dishantmrt02@gmail.com>
2024-09-16 20:52:56 +0530

    1_Variables

--- 1_Basics/1_Variables.js
+++ 1_Basics/1_Variables.js
@@ -1,7 +1,10 @@
 const accountId = 4163
 let accountEmail = "bond007@gmail.com"
 var accountPassword = "1234567"
+//another way to assign var/variable & it is bad practice
 accountCity = "Jaipur"
+
+let accountState;//this let is 'undefined' i.e, no value given to accState therefore it return undefined
 
 //accountId = 2 //since accId is const/constant that cannot be changed which is not allowed
 
@@ -17,4 +20,4 @@ because of issue in block scope i.e., " {} " and functional scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
