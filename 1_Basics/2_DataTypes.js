d7763a9b17e82bb3adb48cc251a88fb386df6d29
Dishant Pal <dishantmrt02@gmail.com>
2024-09-16 21:10:02 +0530

    Data Types

---
+++ 1_Basics/2_DataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //this treat all JS code as newer version 
+
+
+
+// alert(3 + 3) //we are using nodejs(it gives error), not browser
+//in browser inspect->console it return 6->alert(3+3)
+
+
+// code readability should be high
+console.log( 4 
+    +
+    3) //bad practice
+
+console.log(3 + 3); //good practice
+
+console.log("Hello Ji!");
+
