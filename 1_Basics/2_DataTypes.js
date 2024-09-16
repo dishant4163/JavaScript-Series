ee630cd482530f6ea3b84f5be929629e96bb0bc1
Dishant Pal <dishantmrt02@gmail.com>
2024-09-16 21:41:07 +0530

    Data Types

--- 1_Basics/2_DataTypes.js
+++ 1_Basics/2_DataTypes.js
@@ -15,3 +15,57 @@ console.log(3 + 3); //good practice
 
 console.log("Hello Ji!");
 
+
+let name = "Dishant Pal"
+let nickname ="" //string assign, as empty string
+let age = 18
+let loggedIn = false
+let isJob; //variable assigned but value is not given(undefined)
+let stateSalary = null //intenionally I assgin Null ek traha ka empty
+
+
+
+//Types of DataTpes
+/*// JS has 7 primitive data types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => i.e, abhi value assign nhi hui h
+// symbol => unique  
+
+*/
+
+
+/*// JS non-primitive datatypes, or reference type
+
+// Object
+// Array
+// Function
+// Date
+// RegExp
+// Map
+// Set
+// WeakMap
+// WeakSet
+
+*/
+
+
+
+// object
+
+console.log(typeof null); //return Object
+console.log(typeof undefined); //return Undefined
+console.log(typeof "Dishant") //return String
+console.log(typeof 18) //return Number
+
+/* //Use of "typeof Operator":
+// JS typeof operator to find the type of a JavaScript variable
+// typeof operator returns the type of a variable or an expression
+*/
+
+let car = ""; //value is "", & the typeof is "string" 
+
