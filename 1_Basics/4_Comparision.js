65b2bc3a66ec78ece156fb9f5360832dbe2ea7a2
Dishant Pal <dishantmrt02@gmail.com>
2024-09-18 18:52:45 +0530

    Basic of Comparision

---
+++ 1_Basics/4_Comparision.js
@@ -0,0 +1,35 @@
+//Basic of Comparision in JS
+
+
+// Output of all below code in Boolean form 
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+// console.log(2 != 1);
+
+
+//Note:- Problem B/w when not comparing same DataType
+// console.log("2" > 1); // string > number
+// console.log("02" > 1)
+
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+
+// Point to Ponder
+// Reason is that an Equality Check == & comparisions >, <, >=, <= works differently
+
+// Comparisions Convert null to a Number, treating it as 0.
+// i.e., (18) null >= 0 is true & (17) null > 0 is false 
+
+
+
+
+//same case Happens with 'undefined'
+
+
+
