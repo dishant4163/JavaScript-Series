dd9c9d72667f4a74077a926214c5b39d578c1b31
Dishant Pal <dishantmrt02@gmail.com>
2024-09-18 19:01:04 +0530

    Basic of Comparision

--- 1_Basics/4_Comparision.js
+++ 1_Basics/4_Comparision.js
@@ -2,23 +2,23 @@
 
 
 // Output of all below code in Boolean form 
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 2);
-// console.log(2 != 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log(2 != 1);
 
 
 //Note:- Problem B/w when not comparing same DataType
-// console.log("2" > 1); // string > number
-// console.log("02" > 1)
+console.log("2" > 1); // string > number
+console.log("02" > 1)
 
 
-// console.log(null > 0);
-// console.log(null >= 0);
-// console.log(null == 0);
-// console.log(null < 0);
-// console.log(null <= 0);
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null <= 0);
 
 // Point to Ponder
 // Reason is that an Equality Check == & comparisions >, <, >=, <= works differently
@@ -29,7 +29,21 @@
 
 
 
-//same case Happens with 'undefined'
+// same case Happens with 'undefined' 
+// but it always gives false as comparision answer
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log(undefined >= 0);
+console.log(undefined <= 0);
 
 
 
+// Case of triple equality check ' === ' or we call 'Strict Check'
+console.log("2" === 2);
+
+
+
+
+// Note such of Comparision that mention Above
