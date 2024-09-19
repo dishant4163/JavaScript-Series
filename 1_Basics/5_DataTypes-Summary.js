574c0d3be1d0113b96a54331cba94394ed3d526e
Dishant Pal <dishantmrt02@gmail.com>
2024-09-19 22:30:10 +0530

    DataTypes Summary  (Lec: 1 to 4)

--- 1_Basics/5_DataTypes-Summary.js
+++ 1_Basics/5_DataTypes-Summary.js
@@ -1,3 +1,14 @@
-//DataTypes_Summary (Lec: 1 to 4)
+// ******DataTypes_Summary (Lec: 1 to 4)*******
+
+
+/*// Primitive Data Types:-
+1. 	Compared/Call by value
+2.  Stored directly in the stack
+3.  No methods (except for wrapper objects)
+4.  Immutable (cannot be changed)
+*/
+// 7 Primitive Data Types in JS: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
 
 
