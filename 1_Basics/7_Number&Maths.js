d33fbd71ab299664278f7036a597f0731535a18b
Dishant Pal <dishantmrt02@gmail.com>
2024-09-21 19:44:00 +0530

    Number and Maths in JS

--- 1_Basics/7_Number&Maths.js
+++ 1_Basics/7_Number&Maths.js
@@ -5,9 +5,39 @@ const marks = 100
 
 // ' new ' keyword in JavaScript is used to create an instance of an object
 const balance = new Number(150);
-console.log(balance);
+// console.log(balance);
 // convert number into string also checking size
-console.log(balance.toString.length);
+// console.log(balance.toString().length);
+//' toFixed() ' method in JS is used to specify the number of digits 
+// to appear after the decimal point.
+// console.log(balance.toFixed(2)); //give decimal point upto 2 decimal pts.
+
+
+const otherNum = 129.8667
+
+// console.log(otherNum.toPrecision(4));
+/* NOTE:- About toPrecision
+-> Number of significant digits. Must be in the range 1 - 21, inclusive.
+
+quite confusing and interesting to use toPrecision
+*/
+
+
+const hundreds = 1000000
+console.log(hundreds); //comparing 
+
+//'toLocaleString()' convert no. into string
+// i.e, 10000 to 10,000
+console.log(typeof hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-US')); 
+// gives acc. to USA standrad
+console.log(hundreds.toLocaleString('en-IN')); 
+// gives acc. to Indian Standard/number system
+
+
+
+
+// +++++++++******** Maths *********+++++++++++
 
 
 
