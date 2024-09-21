547512fc415b575eaa107e3f5bf0faa2c049ae7c
Dishant Pal <dishantmrt02@gmail.com>
2024-09-21 18:58:59 +0530

    Number and Maths in JS

--- 1_Basics/7_Number&Maths.js
+++ 1_Basics/7_Number&Maths.js
@@ -1,4 +1,13 @@
 // Number and Maths in JS 
 
+const marks = 100
+// console.log(marks);
+
+// ' new ' keyword in JavaScript is used to create an instance of an object
+const balance = new Number(150);
+console.log(balance);
+// convert number into string also checking size
+console.log(balance.toString.length);
+
 
 
