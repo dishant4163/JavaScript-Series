5a54647d516324b1cf413aaec7c3ec78da3becd4
Dishant Pal <dishantmrt02@gmail.com>
2024-09-22 22:18:55 +0530

    Date & Time in JS

--- 1_Basics/8_DateInJS.js
+++ 1_Basics/8_DateInJS.js
@@ -1,10 +1,19 @@
 // Dates
 
-
+// create objec with it instance
 let myDate = new Date()
-console.log(myDate.toString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
 
+
+let myCreatedDate = new Date(2024, 0, 30)
+/* Notes:-
+In JS, date & time months start from 0, ends at 11
+*/
+console.log(myCreatedDate.toDateString());
 
 
 
