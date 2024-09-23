ad9d9ee1ca51cc94614c7a777d80eeeb1ae6879c
Dishant Pal <dishantmrt02@gmail.com>
2024-09-23 21:14:39 +0530

    Array part-1

--- 2_Basics/1_Arrays.js
+++ 2_Basics/1_Arrays.js
@@ -1,2 +1,34 @@
-console.log("Arrays in JS!")
+// Arrays
+
+const myArr = [0, 1, 7, 3, 4, 5]
+const myHeros = ["ironMan", "Thor", "Venom"]
+
+// Another way to represent Array using new keyword
+const myArr2 = new Array(1, 2, 3, 4, 6)
+// console.log(myArr[2]);
+
+
+
+
+// Array methods
+
+// myArr.push(8)
+// myArr.push(11)
+// myArr.pop();
+
+
+
+
+
+console.log(myArr);
+
+
+
+
+
+
+/* Notes:-
+ # In JS, Array can have mix data types like number, strings etc.
+
+*/
 
