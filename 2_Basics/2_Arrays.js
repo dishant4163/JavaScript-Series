86c97a1f9fd084efd314098603f8017ff7beb5ea
Dishant Pal <dishantmrt02@gmail.com>
2024-09-25 21:32:45 +0530

    Arrays Part-2 (objects & array)

--- 2_Basics/2_Arrays.js
+++ 2_Basics/2_Arrays.js
@@ -3,26 +3,26 @@
 const marvel_heros = ["Ironman", "Thor", "Spiderman"]
 const dc_heros = ["Batman", "Superman", "Flash"]
 
-// marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)
 
 //in result array ke undar aaray aayega
-// console.log(marvel_heros); //another array ko bi as data liya h
-// console.log(marvel_heros[3][1]);
+console.log(marvel_heros); //another array ko bi as data liya h
+console.log(marvel_heros[3][1]);
 
 // merging arrays using ' concat '
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
 
 
 // merging arrays using 'spredout operator' good practice
 const all_new_heros = [...marvel_heros, ...dc_heros]
-// console.log(all_new_heros);
+console.log(all_new_heros);
 
 
 const another_Array = [1, 2, 3, [4,5,6], 7, [6,7, [4,5]]]
 const real_another_array = another_Array.flat(Infinity)
 // giving dept as Infinity but we have to define exact dept in numbers
-//console.log(real_another_array);
+console.log(real_another_array);
 
 
 
