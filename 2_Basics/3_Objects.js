5e68c93bc12022a31a36817c844838a136dd7ab4
Dishant Pal <dishantmrt02@gmail.com>
2024-09-25 22:40:18 +0530

    Objects & Array Part-1

--- 2_Basics/3_Objects.js
+++ 2_Basics/3_Objects.js
@@ -9,9 +9,15 @@ Object.create //declare object through constructor method or issme 'singleton' b
 
 // object literals
 //declare objects in Literals
+
+
+const mySym = Symbol("key1") //declaring symbol in objects
+
 const JsUser = {
     //objects me data 'Keys & Values' me define hoti h
     name: "Dishant",
+    "Full Name": "Dishant Pal",
+    [mySym]: "mykey1",//declare as symbol using []
     uid: 4163,
     location: "Mohali",
     email: "user007@gmail.com",
@@ -21,6 +27,50 @@ const JsUser = {
 
 
 //how to Access Objects in diffrent ways
+console.log(JsUser.email); // using ' dot ' 
+console.log(JsUser["email"]); // Notes 3 pt, using square-notation '[]' to access object as string
+
+console.log(JsUser["Full Name"]); //no way to dot or double quotes, so use [" "]'
+
+console.log(JsUser[mySym]); //access symbol using []
+
+
+
+
+
+// How to change Objects Values
+JsUser.email = "user1@hotmail.com"
+console.log(JsUser["email"]);
+
+//we if don't want to allow to change any values in objects
+// Object.freeze(JsUser)
+JsUser.email = "user1@jsmail.com"
+console.log(JsUser);
+
+
+
+
+
+// Adding Functions in Objects
+JsUser.greeting = function() {
+    console.log("Hello JS user");
+}
+
+// now access this Function
+console.log(JsUser.greeting); //it gives undefine due object.freeze code is active
+// if not active then it gives, [Function (anonymous)] i.e, it gives func. ka reference
+
+// console.log(JsUser.greeting());//check results in terminal
+
+
+
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+
+console.log(JsUser.greeting());//check results in terminal
+console.log(JsUser.greetingTwo());
 
 
 
@@ -44,6 +94,8 @@ const JsUser = {
 
 3. Objects me data String me define hota h in machine (vvv imp Point)
 
-4. 'singleton' ye Literals mein nhi ban tha h par constructor se bantha h. Or ye apne thara ka object h
+4. ' this ' keyword is used to reference the same object with 'dot' to gets its property
+
+5. 'singleton' ye Literals mein nhi ban tha h par constructor se bantha h. Or ye apne thara ka object h
 
 */
