bd5ff14e7d846db338e14b6b156657ae934d83be
Dishant Pal <dishantmrt02@gmail.com>
2024-09-25 21:59:07 +0530

    Objects & Array Part-1

--- 2_Basics/3_Objects.js
+++ 2_Basics/3_Objects.js
@@ -1,5 +1,49 @@
-// Objects in Js
+// ********** Objects(& array) in JS Part-1 **********
+
+// singleton (comimg soon...)
+Object.create //declare object through constructor method or issme 'singleton' banega
 
 
 
 
+
+// object literals
+//declare objects in Literals
+const JsUser = {
+    //objects me data 'Keys & Values' me define hoti h
+    name: "Dishant",
+    uid: 4163,
+    location: "Mohali",
+    email: "user007@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Mon", "Fri"] //objects k undar hum array, objects etc kuch bhi value de sakthe h
+}
+
+
+//how to Access Objects in diffrent ways
+
+
+
+
+
+
+
+
+
+
+
+
+/* Objects Notes in JS
+
+1. There are 2 ways to declare Objects:-
+  a). As Literals
+  b). As Constructor
+  c). there are others way too, but these 2 are main
+
+2. Objects/{} k undar Keys & values hota h
+
+3. Objects me data String me define hota h in machine (vvv imp Point)
+
+4. 'singleton' ye Literals mein nhi ban tha h par constructor se bantha h. Or ye apne thara ka object h
+
+*/
