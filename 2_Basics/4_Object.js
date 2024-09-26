a296c962ccf411ab4ae070a68ee3ef5f470a10b5
Dishant Pal <dishantmrt02@gmail.com>
2024-09-26 15:12:34 +0530

    Objects & Array Part-2

--- 2_Basics/4_Object.js
+++ 2_Basics/4_Object.js
@@ -9,7 +9,7 @@ tinderUser.id = "123xyzj"
 tinderUser.name = "Name"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+console.log(tinderUser);
 
 
 
@@ -24,7 +24,7 @@ const regularUser = {
     }
 }
 
-// console.log(regularUser.fullName.userfullName.firstName);
+console.log(regularUser.fullName.userfullName.firstName);
 
 
 
@@ -34,20 +34,20 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 const obj3 = {5: "e", 6: "f"}
 
-//const obj3 = { obj1, obj2} // bad practice to use 
+const obj4 = { obj1, obj2} // bad practice to use 
 // bcz object k object as data lele ga 
 
 
-// const obj4 = Object.assign({}, obj1, obj2, obj3) //good practice to use this syntax
+const obj5 = Object.assign({}, obj1, obj2, obj3) //good practice to use this syntax
 // adding empty '{ }' is good practice due to more value in original object
-// console.log(obj4);
+console.log(obj5);
 
 
 
 
 //but 'assign' is used less so more good practice to use is ' Spread Operator' to merging objects
-const obj5 = {...obj1, ...obj2, ...obj3}
-// console.log(obj5);
+const obj6 = {...obj1, ...obj2, ...obj3}
+console.log(obj6);
 
 
 /* Same things Happens when data/Values comes from the DataBases
@@ -72,7 +72,16 @@ const users = [
 users[1].email
 console.log(tinderUser);
 
-// 
+// Diff. Methods to Fectch Keys-Values form Objects
+console.log(Object.keys(tinderUser)); // Output DataType is Array
+console.log(Object.values(tinderUser)); // Output DataType is Array
+
+// Another method to fectch both Keys-Values but it gives array k undar array as output
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // Result types is Boolean
+
 
 
 
@@ -93,6 +102,6 @@ console.log(tinderUser);
  4.1 Adding empty { }, while merging the objects
  4.2 More Good practice to use 'Spread OPerator' to merging the objects same as done in Arrays
 
-
+5. "hasOwnProperty" is questioner property to ask or check the values is present or not & result is given in Boolean
 
 */
