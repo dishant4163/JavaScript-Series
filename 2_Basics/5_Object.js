d1308649e45f36ae38c4da40dba7b08d721bf9cb
Dishant Pal <dishantmrt02@gmail.com>
2024-09-27 22:51:51 +0530

    Object ( Object De-Structure & JSON API )Part-3

--- 2_Basics/5_Object.js
+++ 2_Basics/5_Object.js
@@ -1,2 +1,22 @@
 // Object de-structure & JSON API intro
 
+
+const course = {
+    courseName: "JS by Chai aur Code",
+    price: "999",
+    courseInstructor: "JS Master"
+}
+
+//print the values of course 
+// course.courseInstructor // this can be & it practice too
+
+// Another way to access/print values
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+
+
+
+/* +****+ Object De-Structure & JSON Api Notes-  +****+
+
+*/
