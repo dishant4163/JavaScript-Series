5da8a4160e592f02a4ab514636f0ebb7275ed775
Dishant Pal <dishantmrt02@gmail.com>
2024-09-28 18:09:24 +0530

    Functions & All its Methods

--- 3_Basics/1_Functions.js
+++ 3_Basics/1_Functions.js
@@ -1,2 +1,36 @@
 // Functions in JS
 
+
+function sayMyName() {
+    console.log("D");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("N");
+    console.log("T");
+}
+
+// how to call function
+// sayMyName() // 'sayMyName' is function ka reference
+// '()' or ye hai function ka Executor
+
+
+
+
+
+
+
+
+
+/* Functions Notes in JS
+
+1. Define/declaration structure of Function
+ # function FunctionName() {
+
+ # }
+
+ # 'keyword' 'function ka name' 'pir ()' us ke bad function ka 'scope/ya function ka defination {}'
+
+
+*/
