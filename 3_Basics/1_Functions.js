97f28a72b4bfc25a2478863d00a21eeef8de69fd
Dishant Pal <dishantmrt02@gmail.com>
2024-09-28 18:35:00 +0530

    Functions & All its Methods

--- 3_Basics/1_Functions.js
+++ 3_Basics/1_Functions.js
@@ -19,14 +19,35 @@ function sayMyName() {
 
 
 // Function to Add 2 numbers
-function addTwoNumbers(numbers1, numbers2){
-    console.log(numbers1 + numbers2);
-}
+// function addTwoNumbers(numbers1, numbers2){
+//     console.log(numbers1 + numbers2);
+// }
 
 //calling Function
-addTwoNumbers() // func. with no arguments gives NAN
-addTwoNumbers(2, 7) // func. with arguments gives result
+// addTwoNumbers() // func. with no arguments gives NAN
+// addTwoNumbers(2, 7) // func. with arguments gives result
 
+// Storing Function in Variables
+// const result = addTwoNumbers(2, 7) //result 9
+// console.log("Result : ", result); //gives 'undefine'
+
+function addTwoNumbers(numbers1, numbers2){
+    // let result = numbers1 + numbers2
+    // return result
+
+    // another way to return result
+    return numbers1 + numbers2
+
+
+    // 'return' k bad function kamm nhi karega acc. to function defination in JS lekin return phele kar saktha h
+    // console.log("pal");
+    
+}
+
+// Storing Function in Variables
+const result = addTwoNumbers(2, 7) //result 9
+
+console.log("Result : ", result); //gives 'defined result' 
 
 
 
@@ -35,13 +56,15 @@ addTwoNumbers(2, 7) // func. with arguments gives result
 
 /* Functions Notes in JS
 
-1. Define/declaration structure of Function
- # function FunctionName() {
+ 1. Define/declaration structure of Function
+  # function FunctionName() {
 
- # }
+  # }
 
- # 'keyword' 'function ka name' 'pir () isme function ke parameters pass hote h' us ke bad function ka 'scope/ya function ka defination {}'
- # or jab Function ko Call karathe h tab 'isko ()' Arguments bola jata h
+  # 'keyword' 'function ka name' 'pir () isme function ke parameters pass hote h' us ke bad function ka 'scope/ya function ka defination {}'
+  # or jab Function ko Call karathe h tab 'isko ()' Arguments bola jata h
 
+
+ 2. We store calling function in variables (line no. -> )
 
 */
