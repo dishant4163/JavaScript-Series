f8ec6c63d40ed828f45a3583990f0bea7fff515d
Dishant Pal <dishantmrt02@gmail.com>
2024-10-01 23:09:16 +0530

    Arrow Function & this keyword

--- 3_Basics/3_Arrow.js
+++ 3_Basics/3_Arrow.js
@@ -13,9 +13,12 @@ const user = {
 }
 
 
-user.welcomeMessage()
-user.username = "default user"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "default user"
+// user.welcomeMessage()
+
+console.log(this); // for this code there is no refering contex in global so it will return empty object '{}'
+
 
 
 
@@ -26,4 +29,6 @@ user.welcomeMessage()
 /*
 1. this keyword current contex ke bare me bata h mtlb current contex ko 'refer' kartha h
  # context simple means 'kis k bare me baat ho rahi h' Or means Values
+
+
 */
