0b09e2c6a240a2704870bb6c97d678cb1481e6a1
Dishant Pal <dishantmrt02@gmail.com>
2024-10-01 22:58:55 +0530

    Arrow Function & this keyword

--- 3_Basics/3_Arrow.js
+++ 3_Basics/3_Arrow.js
@@ -1,2 +1,19 @@
 // Arrow Function & this keyword
 
+
+const user = {
+    username: "dishant",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , wlc to website`);
+        console.log(this);
+        
+    }
+}
+
+
+
+/*
+1. this keyword current contex ke bare me bata h
+*/
