f7efc43048f273b158ec47b111160468eeda2c15
Dishant Pal <dishantmrt02@gmail.com>
2024-10-02 22:04:20 +0530

    Arrow Function & this keyword

--- 3_Basics/3_Arrow.js
+++ 3_Basics/3_Arrow.js
@@ -14,16 +14,41 @@ const user = {
 
 
 // user.welcomeMessage()
-// user.username = "default user"
+// user.username = "default user" // yhan pr current contex change kiya h
 // user.welcomeMessage()
 
-// console.log(this); // for this code there is no refering contex in global so it will return empty object '{}'
+// console.log(this); // for this code there is no refering contex in global so it will return empty object '{}' if above 3 lines code is not working
 
 
 
 
 
 // +++++++ Arrow Function ' () => '
+
+// function chai () {
+//     let username = "Dishant"
+//     console.log(this.username);
+// }
+
+// chai()
+
+
+
+
+// another way to declare function specailly arrow function k through
+
+const chai = function() {
+    let username = "Dishant"
+    console.log(this.username);
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -34,6 +59,9 @@ const user = {
 /*
 1. this keyword current contex ke bare me bata h mtlb current contex ko 'refer' kartha h
  # context simple means 'kis k bare me baat ho rahi h' Or means Values
+
+2. Browser k andar inspect me jab 'console.log(this)' karenge to whan pe empty object/{} as compare node terminal ki jagah 'window' as global object h
+
 
 
 */
