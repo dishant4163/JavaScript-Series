16b363f4185ea1feefe5227418f4cd02c09e56e7
Dishant Pal <dishantmrt02@gmail.com>
2024-10-03 21:56:36 +0530

    iiFE in JS

--- 3_Basics/4_iife.js
+++ 3_Basics/4_iife.js
@@ -3,7 +3,20 @@
 
 
 
+
+
+
 /* Notes on 'iiFE'
 
+ > What is an IIFE?
+ > Definition: An IIFE is a function that runs as soon as it is defined.
+
+
+  # Key Uses of IIFEs
+  i). Scope Isolation:- Creates a new scope to avoid variable collisions
+  ii). Encapsulation:- Keeps variables private, preventing them from being accessed from outside.
+  iii). Avoiding Global Variables:- Helps prevent adding unnecessary variables to the global scope.
+  iv). Module Pattern:- Often used to create modules that bundle related code and functionality.
+  v). Initialization Code:- Useful for running setup code without affecting the global environment.
 
 */
