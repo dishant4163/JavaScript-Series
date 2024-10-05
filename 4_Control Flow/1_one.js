672dd6f1940d8f40344e5168821df512acd0e95e
Dishant Pal <dishantmrt02@gmail.com>
2024-10-05 22:00:42 +0530

    control flow | if-else

--- 4_Control Flow/1_one.js
+++ 4_Control Flow/1_one.js
@@ -12,7 +12,19 @@ if (isUserLoggedIn) {
 
 
 
+/* JS Comparison Operators
 
+== 	   equal to
+===    equal value and equal type
+!= 	   not equal
+!==    not equal value or not equal type
+> 	   greater than
+< 	   less than
+>= 	   greater than or equal to
+<=     less than or equal to
+? 	   ternary operator
+
+*/
 
 
 /* Control/Logic Flow NOTES:-
