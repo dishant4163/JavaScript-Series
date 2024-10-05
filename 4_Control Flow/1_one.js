326f8f769526656e2394807be7b90ea8f66c1e19
Dishant Pal <dishantmrt02@gmail.com>
2024-10-05 22:15:27 +0530

    control flow | if-else

--- 4_Control Flow/1_one.js
+++ 4_Control Flow/1_one.js
@@ -10,12 +10,30 @@ const temperature = 40
 //     console.log("user loggedIn");
 // }
 
-if (temperature === 40) {
-    console.log("Less than 50");
-}  else {
-    console.log("temperature is greter than 50");
-    
+// if (temperature === 40) {
+//     console.log("Less than 50");
+// }  else {
+//     console.log("temperature is greter than 50");
+// }
+
+
+// Quite Interesting thing in js & it is related to scope/{ }
+const score = 420
+
+if(score > 150) {
+    const power = "fly"
+    console.log(`User power: ${power}`); //or ye scope k andar h or inka alag scope count hota h
+    // agr yhan const ki jagah 'var' which is bad pratice 
+    // but ye var scope k bhara bhi access hota
 }
+
+console.log(`User power: ${power}`); //ye scope bhara h tu iska kamm khatam
+
+
+
+
+
+
 
 
 
