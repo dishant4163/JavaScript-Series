246b7679b6f0a12c8926d34021bc7974ae1f20be
Dishant Pal <dishantmrt02@gmail.com>
2024-10-05 21:52:45 +0530

    control flow | if-else

--- 4_Control Flow/1_one.js
+++ 4_Control Flow/1_one.js
@@ -1,8 +1,23 @@
-// Control Flow in JS | (How does javascript execute code + call stack)
+// Control Flow in JS | (How does javascript execute code + call stack) | control-flow aka logic-flow
+
+
+// if
+
+const isUserLoggedIn = true
+
+if (isUserLoggedIn) {
+    console.log("user loggedIn");
+}
 
 
 
 
 
 
+
+/* Control/Logic Flow NOTES:-
+
+
+
+*/
 
