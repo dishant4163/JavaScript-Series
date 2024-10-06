963f3fbf91e828a9306579fdf65ea972f5c14232
Dishant Pal <dishantmrt02@gmail.com>
2024-10-06 22:15:05 +0530

    control flow | if-else

--- 4_Control Flow/1_one.js
+++ 4_Control Flow/1_one.js
@@ -34,9 +34,21 @@ const temperature = 40
 // shortHand Notation aka Implicit scope(bad practice)
 const balance = 1000
 
-if (balance > 150) console.log("test"), console.log("test2");
+// if (balance > 150) console.log("test"), console.log("test2");
 
 
+
+// NESTING Conditions in Logic-Flow
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+} else if (balance < 900) {
+    console.log("less than 750");
+} else {
+    console.log("less than 1200");
+}
 
 
 
