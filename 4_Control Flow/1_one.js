00a842006230e8fa4ac3a12f9f5c5021803f7946
Dishant Pal <dishantmrt02@gmail.com>
2024-10-06 22:19:54 +0530

    control flow | if-else

--- 4_Control Flow/1_one.js
+++ 4_Control Flow/1_one.js
@@ -51,6 +51,16 @@ const temperature = 40
 // }
 
 
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to order food :-) ");
+    
+}
+
 
 
 
