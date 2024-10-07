52e8ff5085db4cc3c26a92a948552dc15fbe6f36
Dishant Pal <dishantmrt02@gmail.com>
2024-10-07 22:19:07 +0530

    switch in JS | control flow

--- 4_Control Flow/2_switch.js
+++ 4_Control Flow/2_switch.js
@@ -14,5 +14,27 @@
 
 
 
+const month = 3
 
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
 
