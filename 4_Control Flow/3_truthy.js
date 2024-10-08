26e606183bc308814a0522d3c91a5fbd997562f6
Dishant Pal <dishantmrt02@gmail.com>
2024-10-08 22:02:54 +0530

    truthy & falsy in JS | control flow

--- 4_Control Flow/3_truthy.js
+++ 4_Control Flow/3_truthy.js
@@ -1,3 +1,18 @@
 // truthy & falsy in JS
 
+const userEmail = "user@email.ai"
 
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+
+
+
+
+/* NOtes on Truthy & Falsy values
+
+
+*/
