9cb1ee047594f09a43e58a5ed65e7ae8f6e79cce
Dishant Pal <dishantmrt02@gmail.com>
2024-10-09 22:43:51 +0530

    Loops in JS

--- 5_Iterations/1_One.js
+++ 5_Iterations/1_One.js
@@ -2,8 +2,15 @@
 
 for (let i=0; i <= 10; i++) {
     const element = i;
+    if(element == 5) {
+        console.log("5 is a number");
+        
+    }
         console.log(element);
 }
+
+// console.log(element);
+
 
 
 
