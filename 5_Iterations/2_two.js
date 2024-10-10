f335938f20e39a31bcc521d3218fcf5a7cadb86d
Dishant Pal <dishantmrt02@gmail.com>
2024-10-10 21:14:16 +0530

    Loops in JS | while

--- 5_Iterations/2_two.js
+++ 5_Iterations/2_two.js
@@ -1,6 +1,21 @@
 // while Loop in js
 
 
+let index = 0
 
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+
+let myAr = ["ironMan", "Black Panther", "Thor"]
+
+let arr = 0
+while (arr < myAr.length) {
+    console.log(`Value is ${myAr[arr]}`);
+    arr = arr + 1 // move to next
+}
 
 
