0a087dba2331ce8d50a5e68616e69da7119209b6
Dishant Pal <dishantmrt02@gmail.com>
2024-10-12 20:18:31 +0530

    forEach Loop

--- 5_Iterations/5_five.js
+++ 5_Iterations/5_five.js
@@ -1,3 +1,19 @@
-// foreach loop in JS
+// forEach loop in JS
 
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// Higer Order Array loops
+coding.forEach( function (val) {
+    console.log(val);
+} )
+
+
+
+
+
+/*
+1. callbackfn:-> this function didn't have name 
+
+*/
 
