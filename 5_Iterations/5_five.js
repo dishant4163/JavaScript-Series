63806011a3a5fbfd9fc4c8ece04d0b5dc71e1616
Dishant Pal <dishantmrt02@gmail.com>
2024-10-12 20:25:03 +0530

    forEach Loop

--- 5_Iterations/5_five.js
+++ 5_Iterations/5_five.js
@@ -4,9 +4,36 @@
 const coding = ["js", "ruby", "java", "python", "cpp"]
 
 // Higer Order Array loops
-coding.forEach( function (val) {
-    console.log(val);
-} )
+
+// types of way for callbackfn
+
+// coding.forEach( function (val) { //here using function(unname function)
+//     console.log(val);
+// } )
+
+
+
+//here using arrow function
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+
+
+//function with name deaclartion
+// function printMe(items) {
+//     console.log(items);
+// }
+
+// coding.forEach(printMe) //here we were giving functions as reference and not executing it & rest print it will do by itself.
+
+
+
+
+
+//const myCoding = 
+
+
 
 
 
