49d7fac4614126bf9053c2e2da06fb6bfee3ef80
Dishant Pal <dishantmrt02@gmail.com>
2024-10-12 19:57:58 +0530

    forof Loop on Object

--- 5_Iterations/4_four.js
+++ 5_Iterations/4_four.js
@@ -1,3 +1,18 @@
 // forof Loop on Object
 
 
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    console.log(key); //only print key info
+    //console.log(myObject[key]); //only print values info
+    //console.log(`${key} shortcut is for ${myObject}`); //print both key & values
+}
+
+
