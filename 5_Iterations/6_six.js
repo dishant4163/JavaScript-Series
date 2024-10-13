0535407d1f3da1a70d237fa057f67cce2caf4221
Dishant Pal <dishantmrt02@gmail.com>
2024-10-13 21:07:16 +0530

    forEach Loop | filter map & reduce

--- 5_Iterations/6_six.js
+++ 5_Iterations/6_six.js
@@ -4,7 +4,8 @@
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
 
-// const values = coding.forEach( (item) => {
+// const values = coding.forEach( (item) => { //storing forEach loop in variable 
+
 //     //console.log(item);
 //     return item
 // })
@@ -13,5 +14,18 @@
 
 
 
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
+
+
+
+
+
+/*
+ 1. forEach koi bhi value return nhi kartha sirf particular callbackfn ka 'undefined' show kartha h manually return karne k bad bhi.
+
+ 2. 
+
+*/
 
