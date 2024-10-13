d64b020dc9276e4debb04839e1df98569bd471ab
Dishant Pal <dishantmrt02@gmail.com>
2024-10-13 21:50:28 +0530

    forEach Loop | filter map & reduce

--- 5_Iterations/6_six.js
+++ 5_Iterations/6_six.js
@@ -14,7 +14,37 @@
 
 
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// filters in JS
+const myNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//storing filter in JS
+// const newNum = myNum.filter( (num) => num > 4)
+// console.log(newNum);
+
+
+
+// Another Way
+
+// const newNum = myNum.filter( (Num) => {
+//     return Num > 4
+// } )
+
+// console.log(newNum);
+
+
+
+
+
+const newNum2 = []
+
+myNum.forEach( (num) => {
+    if (num > 4) {
+        newNum2.push(num)
+    }
+} )
+
+console.log(newNum2);
 
 
 
@@ -22,10 +52,19 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 
-/*
+
+
+
+
+/* NOTES
  1. forEach koi bhi value return nhi kartha sirf particular callbackfn ka 'undefined' show kartha h manually return karne k bad bhi.
 
- 2. 
+ 2. for the above problem in pt. 1, we have filter
+
+ 3. Agar hum filter me scope/{} start kardenge tu  humhe pir return karna padega
+ 4. Agr hum scope/{} open nhi kar h tu direct hi parenthese () ka istamal kar the h tu filter mein direct value return kar denge 
+
+ 5.  Pt. 3 & 4 is can be done with forEach loop in line of code 40 to 
 
 */
 
