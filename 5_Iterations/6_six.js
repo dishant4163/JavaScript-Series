beec1040138982c3fc88ebf3bebe57e7aeee9e81
Dishant Pal <dishantmrt02@gmail.com>
2024-10-14 22:01:43 +0530

    forEach Loop | filter, map & reduce

--- 5_Iterations/6_six.js
+++ 5_Iterations/6_six.js
@@ -59,9 +59,13 @@ const books = [
     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
-  ];
+    ];
 
 let userBooks = books.filter( (bk) => bk.genre === 'History') 
+
+userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === 'Science'
+})
 
 console.log(userBooks);
 
