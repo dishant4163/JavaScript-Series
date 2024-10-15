cdcbb61e8d8d821f8a80893a384002a05ab5ff57
Dishant Pal <dishantmrt02@gmail.com>
2024-10-15 21:45:38 +0530

    map in JS

--- 5_Iterations/7_seven_map.js
+++ 5_Iterations/7_seven_map.js
@@ -1,10 +1,24 @@
 // Map in JS
 
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNumbers.map( (num) => num + 10)
+
+
+console.log(newNums);
+
+
+
 
 
 /* NOTES
 
+ 1. map bhi callbackfn hai in js.
 
+ 2. personal pov, it is better than forEach
 
+ 3. good pt. is that it returns th values
+
+ 4. it can be stored in variables too.
 
 */
