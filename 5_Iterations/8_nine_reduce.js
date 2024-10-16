2153901ca9dbc49553676cf64ebdd6b30f785992
Dishant Pal <dishantmrt02@gmail.com>
2024-10-16 20:51:02 +0530

    reduce in JS

--- 5_Iterations/8_nine_reduce.js
+++ 5_Iterations/8_nine_reduce.js
@@ -1,3 +1,29 @@
 // Reduce in JS
 
+const myNums = [1, 2, 3]
 
+// 4(acc) + 1 + 2 + 3 = 6
+const myTotal = myNums.reduce(function (accumulator, currentvalue) {
+    console.log(`acc: ${accumulator} & curVal: ${currentvalue}`);
+    return accumulator + currentvalue
+}, 4) // 0 is given as accumulator value
+
+console.log(myTotal); //output: 10
+
+
+
+
+
+
+
+
+
+
+
+/* NOTES
+
+ 1. reduce is also callbackfn type in js
+
+ 2. it have 2 variable, i): accumulator, ii): currentvalue
+
+*/
