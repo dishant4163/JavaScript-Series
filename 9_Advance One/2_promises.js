179337947512e95bef7aa3ba220108ad70a9916f
Dishant Pal <dishantmrt02@gmail.com>
2024-10-29 21:37:55 +0530

    promise in JS

--- 9_Advance One/2_promises.js
+++ 9_Advance One/2_promises.js
@@ -1,3 +1,5 @@
+//1. Creating & Storing in Variable
+
 //promis create
 const promiseOne = new Promise(function(resolve, reject){
     //Do an Async task
@@ -11,4 +13,18 @@ const promiseOne = new Promise(function(resolve, reject){
 //now consume promise
 promiseOne.then(function(){
     console.log("Promise consumed");
+})
+
+
+
+
+
+//2. Without Storing in variable(creating & consuming both done in 1-part)
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2")
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved")
 })
