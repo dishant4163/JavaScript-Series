75aeae77bad17f487352b36fe67af18e91d84fb6
Dishant Pal <dishantmrt02@gmail.com>
2024-10-29 21:32:39 +0530

    promise in JS

--- 9_Advance One/2_promises.js
+++ 9_Advance One/2_promises.js
@@ -1,1 +1,14 @@
-const promiseOne = new Promise()
+//promis create
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an Async task
+    //DB calls, cryptography, network related
+    setTimeout(function(){
+        console.log('Async task is Complete');
+        resolve()
+    }, 1000)
+})
+
+//now consume promise
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
