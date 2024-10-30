959ce803443429aa0f3887655bc496da5c2954d6
Dishant Pal <dishantmrt02@gmail.com>
2024-10-30 22:47:23 +0530

    promise in JS

--- 9_Advance One/2_promises.js
+++ 9_Advance One/2_promises.js
@@ -31,3 +31,47 @@ new Promise(function(resolve, reject){
 
 
 
+
+//Passing values from this part 
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        //passing parameters in resolve as object/{}
+        resolve({username: "Dp", email: "chai@example.com"})
+    }, 1000)
+})
+
+//to this part
+promiseThree.then(function(user){ //consuming promise3
+    console.log(user);
+})
+
+
+
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error) {
+            //passing object in resolve
+            resolve({username: "dishant", password: "123"})
+        } else {
+            //Rejection error detha 
+            reject('ERROR: Something went wrong :( ')
+        }
+    }, 1000)
+})
+
+
+//consuming promise4
+// advance step to know (i.e how to avoid callback-Hell)
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => { // Chaining
+    console.log(username);
+}).catch(function(error){ //cathing bcz we were getting error
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved :) or rejected :( "))
+
+
