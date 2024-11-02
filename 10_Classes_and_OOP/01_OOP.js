f160f3e1f81e2ed2be9f54492cc0afa6612b9e77
Dishant Pal <dishantmrt02@gmail.com>
2024-11-02 22:22:22 +0530

    object literal | oop

---
+++ 10_Classes_and_OOP/01_OOP.js
@@ -0,0 +1,11 @@
+// Object Literal (simple object banana => {} ) 
+
+
+//Eg. of Object literal
+const user = {
+    username: "dishant",
+    loginCount: 3,
+    signedIn: true
+}
+
+console.log(user.username)
