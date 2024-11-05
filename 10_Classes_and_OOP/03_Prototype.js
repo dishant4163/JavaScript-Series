215c0593d7b00cd811e5a89a21ca694d91d4271c
Dishant Pal <dishantmrt02@gmail.com>
2024-11-05 22:01:01 +0530

    Prototype in JS

---
+++ 10_Classes_and_OOP/03_Prototype.js
@@ -0,0 +1,45 @@
+// let myName = "dishant     "
+// let mychai = "chaiPiloFriends     "
+
+// console.log(myName.trueLength);
+
+
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spider power is ${this.spiderman}`);
+    }
+}
+
+
+Object.prototype.dishant = function(){
+    console.log(`dishant is present in all objects`);
+}
+
+
+Array.prototype.heydishant = function(){
+    console.log(`DP says Hello Ji! :-)`);
+}
+
+
+// heroPower.dishant()
+// myHeros.dishant()
+// myHeros.heydishant()
+// heroPower.heydishant()
+
+
+
+
+
+// inheritance
+
+
+
