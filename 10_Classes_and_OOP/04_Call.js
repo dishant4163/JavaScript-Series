0ffc5c1fc4fcd1f2884173b941708c91f27e4517
Dishant Pal <dishantmrt02@gmail.com>
2024-11-06 22:05:19 +0530

    call keyword in JS

--- 10_Classes_and_OOP/04_Call.js
+++ 10_Classes_and_OOP/04_Call.js
@@ -1,11 +1,16 @@
 function SetUserName(username) {
     // complex DB calls
     this.username = username
+    console.log("called");
 }
 
 function createUser(username, email, password) {
-    SetUserName(username)
+    SetUserName.call( username)
 
     this.email = email
     this.password = password
 }
+
+const chai = new createUser("chai", "chai@tesla.com", "123")
+console.log(chai);
+
