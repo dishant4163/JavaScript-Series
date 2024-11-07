60d6e0afd1a414e28fce33b481bba5badd0dd32a
Dishant Pal <dishantmrt02@gmail.com>
2024-11-07 21:08:22 +0530

    class in JS

--- 10_Classes_and_OOP/05_myClasses.js
+++ 10_Classes_and_OOP/05_myClasses.js
@@ -2,6 +2,31 @@
 
 
 class User {
-    
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    encryptPassword() {
+        return `${this.password}abc`
+    }
+
+    changeUsername() {
+        return `${this.username.toUpperCase()}`
+    }
+
 }
+
+
+const chai = new User("chai", "chai@fb.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+
+
+// behind the scene
+
 
