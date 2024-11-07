c0fe988950e35f351cf3d07a794a961f9e2e0559
Dishant Pal <dishantmrt02@gmail.com>
2024-11-07 21:16:46 +0530

    class in JS

--- 10_Classes_and_OOP/05_myClasses.js
+++ 10_Classes_and_OOP/05_myClasses.js
@@ -1,32 +1,54 @@
 // This JS series Lectures is madeup after the Launch of "ES6"  
 
 
-class User {
-    constructor(username, email, password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+// class User {
+//     // declaring constructor
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     // declaring own methods
+//     encryptPassword() {
+//         return `${this.password}abc`
+//     }
+//     // declaring own methods
+//     changeUsername() {
+//         return `${this.username.toUpperCase()}`
+//     }
+
+// }
 
 
-    encryptPassword() {
-        return `${this.password}abc`
-    }
+// const chai = new User("chai", "chai@fb.com", "123")
 
-    changeUsername() {
-        return `${this.username.toUpperCase()}`
-    }
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
 
-}
-
-
-const chai = new User("chai", "chai@fb.com", "123")
-
-console.log(chai.encryptPassword());
-console.log(chai.changeUsername());
 
 
 
 // behind the scene
+function User(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
 
+
+// adding/injecting properties in this above mentioned functions
+User.prototype.encryptPassword = function() {
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function() {
+    return `${this.username.toUpperCase()}`
+}
+
+
+// use-Case of this function 4 behind scenes
+const tea = new User("tea", "tea@fb.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
 
